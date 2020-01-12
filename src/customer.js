8aab0d9f8cb68b79afd95cf3bf22bd2af7f10c81
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-01-12 12:19:34 -0700

    Add structure to manager and customer clases

---
+++ src/customer.js
@@ -0,0 +1,18 @@
+class Customer {
+  constructor(id, name) {
+    this.name = name;
+    this.id = id;
+    this.pastBookings = pastBookings;
+    this.futureBookings = futureBookings;
+  }
+
+  selectDate() {
+
+  }
+
+  bookRoom() {
+
+  }
+}
+
+export default Customer;
---
+++ src/manager.js
@@ -0,0 +1,18 @@
+import Customer from './customer.js';
+
+class Manager extends Customer {
+  constructor() {
+    super()
+    
+  }
+
+  deleteBooking() {
+
+  }
+
+  searchUsers() {
+
+  }
+}
+
+export default Manager;
