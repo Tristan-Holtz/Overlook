30db4bb3db929aaffe583977de515e58422debd1
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-01-12 12:20:04 -0700

    Add structure to dashboard class

---
+++ src/dashboard.js
@@ -0,0 +1,18 @@
+class Dashboard {
+  constructor(user) {
+    this.user = user;
+    this.date = Date.now();
+  }
+
+  filterRoom() {
+
+  }
+
+  generateHTML() {
+    $('.main_dashboard').append(`
+
+      `)
+  }
+}
+
+export default Dashboard;
