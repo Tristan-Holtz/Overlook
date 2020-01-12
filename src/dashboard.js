d99757843f85b8f08819a3b66033a704e2262552
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-01-12 13:33:14 -0700

    Create generateHTML template for manager

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 class Dashboard {
   constructor(user) {
     this.user = user;
@@ -8,10 +10,29 @@ class Dashboard {
 
   }
 
-  generateHTML() {
-    $('.main_dashboard').append(`
+  generateHTML(user) {
+    if(user === 'manager') {
+      $('.main_dashboard').append(`
+        <header class="main_header">
+        </header>
+        <section class="manager_dashboard">
+          <aside class="manager_dashboard-options">
+            <form class="form_manager-options">
+              <label class="input_label">Customer ID:</label>
+              <input class="input_customer-id"></input>
+              <label class="input_label">Room Type:</label>
+              <input class="input_room-type"></input>
+              <label class="input_label">Date:</label>
+              <input class="input_date"></input>
+            </form>
+          </aside>
+          <section class="rooms_section">
+          </section>
+        </section>
+        `)
+    } else {
 
-      `)
+    }
   }
 }
 
