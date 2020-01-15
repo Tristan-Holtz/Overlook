c05c0d6b8ed3844ad8258f0ba478e86d3bd358b4
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-01-14 19:48:16 -0700

    Refactor HTML insert functions

--- src/dashboard.js
+++ src/dashboard.js
@@ -9,42 +9,77 @@ class Dashboard {
     this.roomsBookedToday = [];
     this.roomNumbers = [];
     this.totalRevenueToday = 0;
-    this.currentSelection = [];
     this.totalSpent = 0;
     this.roomsAvailableToday = [];
+    this.customerBookings = [];
+  }
+
+  resetTotalSent(name) {
+    this.totalSpent = Math.floor(this.customerBookings.reduce((sum, booking) => {
+      this.rooms.forEach(room => {
+        if(room.number === booking.roomNumber) {
+          sum += room.costPerNight;
+        }
+      })
+      return sum;
+    }, 0))
+  }
+
+  filterByUser(name) {
+    let user = this.user.customerDetails.find(customer => customer.name === name)
+    this.customerBookings = this.bookings.filter(booking => {
+      return booking.userID === user.id;
+    })
+    this.resetTotalSent(name);
+    $('.rooms_section').empty();
+    $('.rooms_section').append(`<h2>${user.name}</h2>`);
+    $('.rooms_section').append(`<h2>Has spent a total of: $${this.totalSpent}</h2>`);
+    this.customerBookings.forEach(booking => {
+      $('.rooms_section').append(`
+          <section>
+            <p>On ${booking.date} room number ${booking.roomNumber} was booked.</p>
+          </section>
+        `);
+    })
+  }
+
+  generateRoomHTML(room) {
+    $('.rooms_section').append(`
+        <article class='rooms_section-card'>
+          <div class='room-card'>
+            <p>Room number ${room.number} is available</p>
+            <p>Cost: $${room.costPerNight}</p>
+            <p>Room-Type: ${room.roomType}</p>
+            <p>Beds: ${room.numBeds}</p>
+            <p>Bed-Size: ${room.bedSize}</p>
+            <p>Bidet: ${room.bidet}</p>
+          </div>
+        </article>
+      `)
   }
 
   filterByDate(date) {
     $('.rooms_section').empty();
+    this.roomsAvailableToday = [];
     if(date !== this.date) {
       this.roomsBookedToday = this.bookings.filter(booking => booking.date === date);
       this.roomNumbers = this.roomsBookedToday.map(room => room.roomNumber);
     }
     this.rooms.forEach(room => {
-        if(!this.roomNumbers.includes(room.number)) {
-          this.roomsAvailableToday.push(room);
-          $('.rooms_section').append(`
-              <article class='rooms_section-card'>
-                <div>
-                  ${room.number}
-                </div>
-              </article>
-            `)
-        }
+      if(!this.roomNumbers.includes(room.number)) {
+        this.roomsAvailableToday.push(room);
+        this.generateRoomHTML(room);
+      }
     })
   }
 
   filterByRoom(type) {
     $('.rooms_section').empty();
-    let currentRooms = this.roomsAvailableToday.filter(room => room.type === type);
+    let currentRooms = this.roomsAvailableToday.filter(room => {
+      return room.roomType === type});
+    this.roomsAvailableToday = currentRooms;
     currentRooms.forEach(room => {
-      $('.rooms_section').append(`
-          <article class='rooms_section-card'>
-            <div>
-              ${room.number}
-            </div>
-          </article>
-        `)
+      this.generateRoomHTML(room);
     })
   }
 
@@ -68,7 +103,7 @@ class Dashboard {
           <aside class="manager_dashboard-options">
             <form class="form_manager-options">
               <label class="input_label">Customer Name:</label>
-              <input class="input_customer-id" placeholder="Enter customer name"></input>
+              <input class="input_customer-name" placeholder="Enter customer name"></input>
               <button type="button" class="input_customer-button">GO</button>
               <label class="input_label">Room Type:</label>
               <input class="input_room-type" placeholder="Enter room type"></input>
@@ -100,10 +135,10 @@ class Dashboard {
       $('.main_dashboard').append(`
         <header class="main_header">
         </header>
-        <h2>Welcome back ${user.name}!</h2>
-        <section>
-          <aside>
-            <h2>Book a new stay!</h2>
+        <h2 class="welcome-message">Welcome back ${user.name}!</h2>
+        <section class="customer_dashboard">
+          <aside class="customer_dashboard-options">
+            <h2>Book a new stay</h2>
             <form>
               <label class="input_label">Room Type:</label>
               <input class="input_room-type" placeholder="Enter room type"></input>
@@ -117,8 +152,8 @@ class Dashboard {
               <p>$${Math.floor(this.totalSpent)}</p>
             </div>
             <div>
-            <h3>User Bookings</h3>
-            <p>
+              <h3>User Bookings</h3>
+              <p></p>
             </div>
           </aside>
           <section class="rooms_section">
